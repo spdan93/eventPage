a40c858948af782712556a0742b70e3f35bec21b
Daniel Pinheiro <danielsp_93@hotmail.com>
2023-09-17 23:16:41 -0300

    login

--- src/js/login.js
+++ src/js/login.js
@@ -2,8 +2,9 @@ function entrar() {
     let senha = document.querySelector('#senha');
 
     if (senha.value == "rarosmomentos") {
-        window.location.href = '../view/.html';
-        // window.open('../view/home.html', '_blank');
+        // window.location.href="http://localhost:5500/src/view/home.html";
+        window.open('http://localhost:5500/src/view/home.html');
+        // window.location.replace("http://localhost:5500/src/view/home.html");
 
         let mathRandom = Math.random().toString(16).substr(2)
         let token = mathRandom + mathRandom
